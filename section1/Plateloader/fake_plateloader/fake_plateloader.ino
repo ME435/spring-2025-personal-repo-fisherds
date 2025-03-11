4cca743efe7a91e41fe93fe6aba0ecd91095bf35
David Fisher <fisherds@rose-hulman.edu>
2025-03-11 09:13:11 -0400

    finished Arduino fake plate loader

--- section1/Plateloader/fake_plateloader/fake_plateloader.ino
+++ section1/Plateloader/fake_plateloader/fake_plateloader.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
+String inputString = "";
+bool stringComplete = false;
 
+void setup() {
+  Serial.begin(19200);
+  inputString.reserve(200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (stringComplete) {
+    if (inputString.equals("RESET")) {
+      delay(1000);
+      Serial.println("READY,  SAGIAN PE Loader, ROM Ver. 1.1.6, 12APR2001");
+    } else if (inputString.startsWith("MOVE ")) {
+      delay(3000);  // Simulated time delay
+      Serial.println("READY");
+    } else if (inputString.equals("Z-AXIS EXTEND")) {
+      Serial.println("READY, EXTENDED");
+    } else if (inputString.equals("Z-AXIS RETRACT")) {
+      Serial.println("READY, RETRACTED");
+    } else if (inputString.equals("GRIPPER OPEN")) {
+      Serial.println("READY, OPEN");
+    } else if (inputString.equals("GRIPPER CLOSE")) {
+      Serial.println("READY, CLOSED, NOPLATE");
+    } else if (inputString.startsWith("X-AXIS ")) {
+      Serial.println("READY");
+    } else if (inputString.startsWith("SET_DELAY ")) {
+      Serial.println("This simulator doesn't do that!");
+    } else if (inputString.equals("LOADER_STATUS")) {
+      Serial.println("READY, POSITION 3, Z-AXIS RETRACTED, GRIPPER CLOSED, NOPLATE");
+    } else {
+      Serial.print("Unknown command --> ");
+      Serial.println(inputString);
+    }
+    inputString = "";
+    stringComplete = false;
+  }
+}
 
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') {
+      stringComplete = true;
+    } else {
+      inputString += inChar;
+    }
+  }
 }
