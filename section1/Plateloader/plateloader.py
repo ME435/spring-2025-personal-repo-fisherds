2d771e2e9526dbdde9cf947df754fc524cda571e
David Fisher <fisherds@rose-hulman.edu>
2025-03-11 09:44:52 -0400

    start of a Plateloader test program

--- section1/Plateloader/plateloader.py
+++ section1/Plateloader/plateloader.py
@@ -11,8 +11,26 @@ class Plateloader:
         if not self.is_connected:
             self.connect()
         # TODO: Actually send the command and return the response
+        return "FAKE RESPONSE"
+    
     
 if __name__ == "__main__":
     print("Running a Plateloader test program")
-        
+    plateloader = Plateloader()
+    plateloader.connect()
+    while True:
+        response = ""
+        print("\n\n0. Exit")
+        print("1. RESET")
+        print("2. X-AXIS")
+        selection = input("Select an option: ")
+        if selection == "0":
+            break
+        elif selection == "1":
+            response = plateloader.send_command("RESET")
+        else:
+            print("Invalid selection", selection)
+        print("Response:", response)
+    
+    print("Goodbye!")
         
