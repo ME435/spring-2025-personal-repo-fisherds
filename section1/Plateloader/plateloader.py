3daf719053aca4c0c2c6d3083bd6512dc50d3c51
David Fisher <fisherds@rose-hulman.edu>
2025-03-18 09:30:46 -0400

    finished in-class python serial menu

--- section1/Plateloader/plateloader.py
+++ section1/Plateloader/plateloader.py
@@ -1,18 +1,37 @@
+import serial
+import time
+
 class Plateloader:
     def __init__(self):
         self.is_connected = False
         self.ser = None
         
-    def connect(self, name="/dev/tty.usbmodem1201"):
-        # TODO: Actually connect to the serial port
+    def connect(self, port="/dev/cu.usbmodem11301"):
+        self.ser = serial.Serial(port, baudrate=19200)
+        print("Connecting... ", end="")
+        while not self.ser.is_open:
+            time.sleep(0.1)
+        print("Connected!")
+        time.sleep(1)
+        self.ser.reset_input_buffer()
         self.is_connected = True
         
     def send_command(self, command):
         if not self.is_connected:
             self.connect()
-        # TODO: Actually send the command and return the response
-        return "FAKE RESPONSE"
-    
+        # Sending the message
+        message_bytes = (command + "\n").encode()
+        print("Sending:", message_bytes)
+        self.ser.write(message_bytes)
+        
+        # Waiting for the response
+        while self.ser.in_waiting == 0:
+            time.sleep(0.1)
+        while self.ser.in_waiting > 0:
+            received = self.ser.readline()
+            print("Received: ", received.decode().strip())
+        return received
+            
     
 if __name__ == "__main__":
     print("Running a Plateloader test program")
@@ -28,6 +47,9 @@ if __name__ == "__main__":
             break
         elif selection == "1":
             response = plateloader.send_command("RESET")
+        elif selection == "2":
+            to_where = input("Enter the X-axis position: ")
+            response = plateloader.send_command(f"X-AXIS {to_where}")
         else:
             print("Invalid selection", selection)
         print("Response:", response)
