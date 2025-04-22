f88119c4c1a0b626a48b2738dc9d5a619a958108
Dave Fisher <fisherds@rose-hulman.edu>
2025-04-22 08:50:40 -0400

    placeholders to prepare

---
+++ section1/Rosebot/rosebot.py
@@ -0,0 +1,6 @@
+from rosebot_drive_system import DriveSystem
+
+class RoseBot:
+    
+    def __init__(self):
+        self.drive_system = DriveSystem()
---
+++ section1/Rosebot/rosebot_drive_system.py
@@ -0,0 +1,12 @@
+from motor import OrdinaryCar
+
+class DriveSystem:
+    
+    def __init__(self):
+        self.car = OrdinaryCar()
+    
+    def go(self, left_speed, right_speed):
+        print("TODO: Use self.car to GO")
+    
+    def stop(self):
+        print("TODO: Use self.car to STOP")
