f6b9235ee9821c51d51bbef09de80ac800df9463
Dave Fisher <fisherds@rose-hulman.edu>
2025-05-05 11:46:59 -0400

    progress on LEDs

---
+++ section2/Rosebot/rosebot_leds.py
@@ -0,0 +1,27 @@
+from spi_ledpixel import Freenove_SPI_LedPixel
+import time
+
+class CarLeds:
+    
+    def __init__(self):
+        self.neopixel_strip = Freenove_SPI_LedPixel(bright=128)
+
+    def set_all_leds(self, red, green, blue):
+        self.neopixel_strip.set_all_led_color(red, green, blue)        
+
+    def set_led(self, led_index, red, green, blue):
+        self.neopixel_strip.set_led_color(led_index, red, green, blue)        
+
+    def turn_off(self):
+        self.set_all_leds(0, 0, 0)
+
+
+if __name__ == "__main__":
+    car_leds = CarLeds()
+    car_leds.set_all_leds(255, 0, 0)  # Set all LEDs to red
+    time.sleep(1)                     # Wait for 1 second
+    car_leds.set_led(0, 0, 255, 0)     # Set LED at index 0 to green
+    time.sleep(1)                     # Wait for 1 second
+    car_leds.set_led(1, 0, 0, 255)     # Set LED at index 1 to blue
+    time.sleep(4)                     # Wait for 1 second
+    car_leds.turn_off()                # Turn off all LEDs
